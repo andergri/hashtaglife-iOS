1d558baaff7ce08cba47f26179386a890f5f3f9e
andergri <andergri1@gmail.com>
2014-07-19 13:16:53 -0400

    Initial Commit

---
+++ selfie/SELAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SELAppDelegate.h
+//  selfie
+//
+//  Created by Griffin Anderson on 7/19/14.
+//  Copyright (c) 2014 Griffin Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SELAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ selfie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  selfie
+//
+//  Created by Griffin Anderson on 7/19/14.
+//  Copyright (c) 2014 Griffin Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SELAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SELAppDelegate class]));
+    }
+}
